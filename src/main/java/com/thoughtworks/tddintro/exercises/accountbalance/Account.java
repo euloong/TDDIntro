6ba1438f36a4c5b75a922bf40971439ebfdcbc9c
euloong <euloong@hotmail.com>
2020-08-13 14:52:44 +1000

    created tests and code for account exercise

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -1,4 +1,16 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
 public class Account {
+    public Integer deposit(int accountBalance, int depositAmount) {
+        accountBalance = accountBalance + depositAmount;
+        return accountBalance;
+    }
+
+    public Integer withdrawal(int accountBalance, int withdrawalAmount) {
+        if (withdrawalAmount <= accountBalance) {
+            accountBalance = accountBalance - withdrawalAmount;
+        }
+        return accountBalance;
+    }
+
 }
--- src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
@@ -1,24 +1,28 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
+import com.thoughtworks.tddintro.exercises.factorial.Factorial;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class AccountTests {
     @Test
-    @Ignore  // Remove each @Ignore and implement test
     public void shouldIncreaseMyBalanceWhenIDepositMoney(){
         Account account = new Account();
+        assertThat(account.deposit(100, 50), is(150));
     }
 
     @Test
-    @Ignore  // Remove each @Ignore and implement test
     public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
-
+        Account account = new Account();
+        assertThat(account.withdrawal(100, 50), is(50));
     }
 
     @Test
-    @Ignore  // Remove each @Ignore and implement test
     public void shouldNotDecreaseMyBalanceWhenIWithdrawMoneyAndDoNotHaveEnoughToCoverTheWithdrawal(){
-
+        Account account = new Account();
+        assertThat(account.withdrawal(50, 100), is(50));
     }
 }
